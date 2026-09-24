0bc22cf47f1e7b3e13c559dd2803966aa7363431
Adelfa-deVera <ccdevera@carc.pshs.edu.ph>
2026-09-24 16:41:10 +0800

    Add files via upload

--- deVera_Christen_DistanceProgram.py
+++ deVera_Christen_DistanceProgram.py
@@ -4,7 +4,9 @@ x1 = int(input("Enter x1: "))
 y1 = int(input("Enter y1: "))
 x2 = int(input("Enter x2: "))
 y2 = int(input("Enter y2: "))
+# Asks the user to enter the variables.
 
 distance = math.sqrt(math.pow(x1-x2,2)+math.pow(y1-y2,2))
+# Calculates the distance between the entered points using sqrt() and pow()
 
 print(f"The distance between the two points is: {distance:.2f}")
